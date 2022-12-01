fcf7e182ff0dc5e8205269faa2e55fd46f04f7d5
surroundsound5000 <44852447+surroundsound5000@users.noreply.github.com>
2022-12-01 07:24:52 +0000

    Set up schedule db and dynamic index pg loading

--- app.py
+++ app.py
@@ -6,8 +6,6 @@ from flask_session import Session
 
 app = Flask(__name__)
 
-
-
 # Configure session to use filesystem (instead of signed cookies)
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
@@ -24,7 +22,10 @@ def get_db_connection():
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    conn = get_db_connection()
+    schedule = conn.execute('SELECT * FROM schedule').fetchall()
+    conn.close()
+    return render_template('index.html', schedule = schedule)
     
 @app.route("/login", methods=["GET", "POST"])
 def login():
